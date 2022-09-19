d532d175eca883dd01213d871727f661df3360d0
Oriovaldo Fialho <oriovaldof@gmail.com>
2022-09-19 13:11:11 -0400

    instalando e utilizando a lid moment de forma local no compomente

---
+++ src/components/Formulario.vue
@@ -0,0 +1,378 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-6 bg-light">
+        <span class="fs-4">ENTRADA DE DADOS</span>
+        <hr />
+        <form>
+          <div class="mb-3 row">
+            <label class="col-3 col-form-label">Nome:</label>
+            <div class="col">
+              <input type="text" class="form-control" v-model.lazy="form.nome" />
+            </div>
+          </div>
+          <div class="mb-3 row">
+            <label class="col-3 col-form-label">E-mail:</label>
+            <div class="col">
+              <input type="email" class="form-control" v-model="form.email" />
+            </div>
+          </div>
+          <div class="mb-3 row">
+            <label class="col-3 col-form-label">Senha:</label>
+            <div class="col">
+              <input type="password" class="form-control" v-model.trim="form.senha" />
+            </div>
+          </div>
+          <div class="mb-3 row">
+            <label class="col-3 col-form-label">Idade:</label>
+            <div class="col">
+              <input type="number" class="form-control" v-model.number="form.idade" />
+            </div>
+          </div>
+          <div class="mb-3 row">
+            <label class="col-3 col-form-label">Gênero:</label>
+            <div class="col">
+              <div class="form-check form-check-inline">
+                <input class="form-check-input" type="radio" value="Feminino" v-model="form.genero"/>
+                <label class="form-check-label"> Feminino </label>
+              </div>
+              <div class="form-check form-check-inline">
+                <input class="form-check-input" type="radio" value="Masculino" v-model="form.genero"/>
+                <label class="form-check-label"> Masculino </label>
+              </div>
+            </div>
+          </div>
+
+          <div class="mb-3 row">
+            <label class="col-3 col-form-label">Licença:</label>
+            <div class="col">
+              <div class="form-check form-switch">
+                <input class="form-check-input" 
+                type="checkbox" 
+                v-model="form.licenca"
+                true-value="SIM"
+                false-value="NAO"
+                />
+                <label class="form-check-label">Li e aceito os termos</label>
+              </div>
+            </div>
+          </div>
+
+          <div class="mb-3 row">
+            <label class="col-3 col-form-label">Interesses:</label>
+            <div class="col">
+              <div class="form-check">
+                <input class="form-check-input" type="checkbox" v-model="form.interesses" value="JavaScript"/>
+                <label class="form-check-label"> JavaScript </label>
+              </div>
+              <div class="form-check">
+                <input class="form-check-input" type="checkbox" v-model="form.interesses"  value="VueJS"/>
+                <label class="form-check-label"> VueJS </label>
+              </div>
+              <div class="form-check">
+                <input class="form-check-input" type="checkbox" v-model="form.interesses" value="Angular" />
+                <label class="form-check-label"> Angular </label>
+              </div>
+              <div class="form-check">
+                <input class="form-check-input" type="checkbox" v-model="form.interesses"  value="NodeJS"/>
+                <label class="form-check-label"> NodeJS </label>
+              </div>
+            </div>
+          </div>
+          <div class="mb-3 row">
+            <label class="col-3 col-form-label">Telefone:</label>
+            <div class="col">
+              <input
+                type="text"
+                class="form-control"
+                v-maska="'(##) #####-####'"
+                v-model="form.telefone"
+              />
+              <small class="text-muted">Formato: 11 97777-5555</small>
+            </div>
+          </div>
+          <div class="mb-3 row">
+            <label class="col-3 col-form-label">CEP:</label>
+            <div class="col">
+              <input
+                type="text"
+                class="form-control"
+                v-maska="'#####-###'"
+                v-model="form.cep"
+              />
+              <small class="text-muted">Formato: 00000-000</small>
+            </div>
+          </div>
+          <div class="mb-3 row">
+            <label class="col-3 col-form-label">CPF:</label>
+            <div class="col">
+              <input
+                type="text"
+                class="form-control"
+                v-maska="'###.###.###-##'"
+                v-model="form.cpf"
+              />
+              <small class="text-muted">Formato: 000.000.000-00</small>
+            </div>
+          </div>
+          <div class="mb-3 row">
+            <label class="col-3 col-form-label">CNPJ:</label>
+            <div class="col">
+              <input
+                type="text"
+                class="form-control"
+                v-maska="'##.###.###/####-##'"
+                v-model="form.cnpj"
+              />
+              <small class="text-muted">Formato: 00.000.000/0000-00</small>
+            </div>
+          </div>
+          <div class="mb-3 row">
+            <label class="col-3 col-form-label">Cartão de Credito:</label>
+            <div class="col">
+              <input
+                type="text"
+                class="form-control"
+                v-maska="'#### #### #### ####'"
+                v-model="form.cartaoDeCredito"
+              />
+              <small class="text-muted">Formato: 0000 0000 0000 0000</small>
+            </div>
+          </div>
+          <div class="mb-3 row">
+            <label class="col-3 col-form-label">Placa Veiculo:</label>
+            <div class="col">
+              <input
+                type="text"
+                class="form-control"
+                v-maska="'AAA-####'"
+                v-model="form.placaVeiculo"
+              />
+              <small class="text-muted">Formato: AAA-0000</small>
+            </div>
+          </div>
+          <div class="mb-3 row">
+            <label class="col-3 col-form-label">Placa Veiculo Mercosul:</label>
+            <div class="col">
+              <input
+                type="text"
+                class="form-control"
+                v-maska="'AAA#A##'"
+                v-model="form.placaVeiculoMercosl"
+              />
+              <small class="text-muted">Formato: AAA0A00</small>
+            </div>
+          </div>
+          <div class="mb-3 row">
+            <label class="col-3 col-form-label">RG:</label>
+            <div class="col">
+              <input
+                type="text"
+                class="form-control"
+                v-maska="'#*-X'"
+                v-model="form.rg"
+              />
+              <small class="text-muted">Formato: sem Padrao</small>
+            </div>
+          </div>
+
+          <div class="mb-3 row">
+            <label class="col-3 col-form-label">Data:</label>
+            <div class="col">
+              <input type="date" class="form-control" v-model="form.data"/>
+            </div>
+          </div>
+          <div class="mb-3 row">
+            <label class="col-3 col-form-label">Data/hora local:</label>
+            <div class="col">
+              <input type="datetime-local" class="form-control" v-model="form.dataHoraLocal"/>
+            </div>
+          </div>
+          <div class="mb-3 row">
+            <label class="col-3 col-form-label">Mês:</label>
+            <div class="col">
+              <input type="month" class="form-control" v-model="form.mes" />
+            </div>
+          </div>
+          <div class="mb-3 row">
+            <label class="col-3 col-form-label">Semana:</label>
+            <div class="col">
+              <input type="week" class="form-control" v-model="form.semana" />
+            </div>
+          </div>
+          <div class="mb-3 row">
+            <label class="col-3 col-form-label">Hora:</label>
+            <div class="col">
+              <input type="time" class="form-control" v-model="form.hora" />
+            </div>
+          </div>
+          <div class="mb-3 row">
+            <label class="col-3 col-form-label">Cor:</label>
+            <div class="col">
+              <input type="color" class="form-color" />
+            </div>
+          </div>
+          <div class="mb-3 row">
+            <label class="col-3 col-form-label">Valor limite:</label>
+            <div class="col">
+              <input
+                type="range"
+                class="form-range"
+                min="0"
+                max="100"
+                step="1"
+              />
+            </div>
+          </div>
+          <div class="mb-3 row">
+            <label class="col-3 col-form-label">Escondido:</label>
+            <div class="col">
+              <input type="hidden" class="form-control" />
+            </div>
+          </div>
+          <div class="mb-3 row">
+            <label class="col-3 col-form-label">Upload:</label>
+            <div class="col">
+              <input type="file" class="form-control" />
+            </div>
+          </div>
+          <hr />
+          <div class="mb-3 row">
+            <div class="col d-flex justify-content-between">
+              <button class="btn btn-secondary" type="reset">Limpar</button>
+              <button class="btn btn-success" type="button">
+                Enviar (btn)
+              </button>
+              <button class="btn btn-success" type="submit">
+                Enviar (submit)
+              </button>
+            </div>
+          </div>
+        </form>
+      </div>
+
+      <div class="col-6 text-white bg-secondary">
+        <span class="fs-4">ESTADO DO OBJETO</span>
+        <hr />
+        <div class="mb-5 row">
+          <span>{{form}}</span>
+        </div>
+
+        <span class="fs-4">SAÍDA DE DADOS</span>
+        <hr />
+        <div class="mb-3 row">
+          <span>Nome:{{ form.nome }}</span>
+        </div>
+        <div class="mb-3 row">
+          <span>E-mail:{{ form.email }}</span>
+        </div>
+        <div class="mb-3 row">
+          <span>Senha:{{ form.senha }}</span>
+        </div>
+        <div class="mb-3 row">
+          <span>Idade:{{ form.idade }}</span>
+        </div>
+        <div class="mb-3 row">
+          <span>Gênero:{{form.genero}}</span>
+        </div>
+        <div class="mb-3 row">
+          <span>Licença:{{form.licenca}}</span>
+        </div>
+        <div class="mb-3 row">
+          <span>Interesses:</span>
+          <ul>
+            <li v-for="(interesse, index) in form.interesses" :key="index">{{interesse}}</li>
+          </ul>
+        </div>
+        <div class="mb-3 row">
+          <span>Telefone:{{form.telefone}}</span>
+        </div>
+        <div class="mb-3 row">
+          <span>CEP:{{form.cep}}</span>
+        </div>
+        <div class="mb-3 row">
+          <span>CPF:{{form.cpf}}</span>
+        </div>
+        <div class="mb-3 row">
+          <span>CNPJ:{{form.cnpj}}</span>
+        </div>
+        <div class="mb-3 row">
+          <span>Cartão de Credito:{{form.cartaoDeCredito}}</span>
+        </div>
+        <div class="mb-3 row">
+          <span>placaVeiculo:{{form.placaVeiculo}}</span>
+        </div>
+        <div class="mb-3 row">
+          <span>placaVeiculoMercosl:{{form.placaVeiculoMercosl}}</span>
+        </div>
+        <div class="mb-3 row">
+          <span>rg:{{form.rg}}</span>
+        </div>
+
+        <div class="mb-3 row">
+          <span>Data:{{form.data}} | {{moment(form.data).format('DD/MM/YYYY')}}</span>
+        </div>
+        <div class="mb-3 row">
+          <span>Data/hora local:{{form.dataHoraLocal}}</span>
+        </div>
+        <div class="mb-3 row">
+          <span>Mês:{{form.mes}}</span>
+        </div>
+        <div class="mb-3 row">
+          <span>Semana:{{form.semana}}</span>
+        </div>
+        <div class="mb-3 row">
+          <span>Hora:{{form.hora}}</span>
+        </div>
+        <div class="mb-3 row">
+          <span>Cor:</span>
+        </div>
+        <div class="mb-3 row">
+          <span>Valor limite:</span>
+        </div>
+        <div class="mb-3 row">
+          <span>Escondido:</span>
+        </div>
+        <div class="mb-3 row">
+          <span>Upload:</span>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import moment from 'moment'
+  // console.log(moment);
+export default {
+  name: "Formulario",
+  data: () => ({
+    moment:{},
+    form: {
+      nome: "Oriovaldo",
+      email: "oriovaldof@gmail.com",
+      senha: "123",
+      idade: 42,
+      licenca:'NAO',
+      interesses:[],
+      genero:'',
+      telefone:'',
+      cep:'',
+      cpf:'',
+      cnpj:'',
+      cartaoDeCredito:'',
+      placaVeiculo: '',
+      placaVeiculoMercosl: '',
+      rg: '',
+      data: '',
+      dataHoraLocal:'',
+      mes:'',
+      semana:'',
+      hora:''
+    },
+  }),
+  created(){
+    this.moment = moment;
+  }
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,11 @@
 import { createApp } from 'vue'
+import Maska from 'maska'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+// createApp(App).mount('#app')
+
+const app = createApp(App);
+
+app.use(Maska);
+
+app.mount('#app')
